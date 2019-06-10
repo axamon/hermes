8ee76ecf21fc0f62d13bd39c8b857aed3a7438f2
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2019-06-10 14:28:36 +0200

    pseudocode added

---
+++ webapp/client/main.go
@@ -0,0 +1,5 @@
+// il client serve a riconoscere la creazione di nuovi log
+// tramite un watchdog
+// a riconocere il tipo di log e a parsarlo di conseguenza
+// Una volta sanitarizzato il log viene inviato a una destinazione
+// prefissata
---
+++ webapp/server/main.go
@@ -0,0 +1,2 @@
+// il server provvede un endpoint a cui il client può
+// inviare i log sanitarizzati.

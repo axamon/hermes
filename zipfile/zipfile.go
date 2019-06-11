5055a434924a879a0a78772d1f565c4c68583082
axamon <alberto.bregliano@pm.me>
2019-06-11 15:11:44 +0200

    licensed

--- zipfile/zipfile.go
+++ zipfile/zipfile.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2019 Alberto Bregliano
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 package zipfile
 
 import (
@@ -8,7 +28,7 @@ import (
 )
 
 // ReadAll legge il file zippato passato come parametro e restituisce
-// il suo contenuto ed eventuale errore.
+// un io.Reader e un eventuale errore.
 func ReadAll(zipFile string) (content io.Reader, err error) {
 
 	// Apre il file zippato in lettura.
@@ -23,11 +43,6 @@ func ReadAll(zipFile string) (content io.Reader, err error) {
 	if err != nil {
 		log.Printf("Errore Impossibile leggere il contenuto del file %s: %s\n", zipFile, err.Error())
 	}
-
-	// content, err = ioutil.ReadAll(gr)
-	// if err != nil {
-	//	log.Printf("Errore Impossibile prelevare contenuti del file file %s: %s\n", zipFile, err.Error())
-	// }
 
 	return gr, err
 }

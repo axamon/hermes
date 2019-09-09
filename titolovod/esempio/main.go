87fc3d38214afd5cc2f758bf2d49071333d38705
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2019-09-09 20:03:17 +0200

    aggiunto controllo titolo

---
+++ titolovod/esempio/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/axamon/hermes/titolovod"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	//var result = new(titolovod.Response)
+	var err error
+
+	idvideoteca := os.Args[1]
+
+	result, err := titolovod.Get(ctx, idvideoteca)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(result[0].ResultObj.Containers[0].Metadata.Title)
+}
---
+++ titolovod/titolovod.go
@@ -0,0 +1,99 @@
+package titolovod
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const searchURLbeginning = "https://www.timvision.it/TIM/10.14.11/PROD/IT/CUBOWEB/ITALY/DETAILS?contentId="
+const searchURLending = "&deviceType=ALL&serviceName=ALL&type=ALL"
+
+type Response []struct {
+	ResultCode string        `json:"resultCode"`
+	SystemTime int           `json:"systemTime"`
+	Errors     []interface{} `json:"errors"`
+	ResultObj  struct {
+		ID         string `json:"id"`
+		Containers []struct {
+			Actions []struct {
+				Key        string `json:"key"`
+				URI        string `json:"uri"`
+				TargetType string `json:"targetType"`
+			} `json:"actions"`
+			Layout   string `json:"layout"`
+			Metadata struct {
+				ShortDescription      string      `json:"-"`
+				ParentalControlLevel  string      `json:"parentalControlLevel"`
+				Language              string      `json:"language"`
+				ContentID             string      `json:"contentId"`
+				ExpirationDate        int         `json:"expirationDate"`
+				Title                 string      `json:"title"`
+				ContentAnalyticsType  string      `json:"contentAnalyticsType"`
+				Type                  string      `json:"type"`
+				LongDescription       string      `json:"longDescription"`
+				Year                  string      `json:"year"`
+				ContentProvider       string      `json:"contentProvider"`
+				Duration              int         `json:"duration"`
+				Genre                 string      `json:"genre"`
+				SupportedDevices      []string    `json:"supportedDevices"`
+				CategoriesThemeArea   []string    `json:"categoriesThemeArea"`
+				Rating                string      `json:"rating"`
+				Badge                 string      `json:"badge"`
+				OfferTypeLabel        interface{} `json:"offerTypeLabel"`
+				ImageURL              string      `json:"imageUrl"`
+				ImageURLOther         string      `json:"imageUrlOther"`
+				BgImageURL            string      `json:"bgImageUrl"`
+				DisplayExpirationDate string      `json:"displayExpirationDate"`
+				Directors             []string    `json:"directors"`
+				Actors                []string    `json:"actors"`
+				BroadcastDisplayDate  string      `json:"broadcastDisplayDate"`
+				ObjectSubtype         string      `json:"objectSubtype"`
+				ObjectType            string      `json:"objectType"`
+				TitleBrief            string      `json:"titleBrief"`
+				VideoType             []string    `json:"videoType"`
+			}
+		}
+	}
+}
+
+// Get recupera i dati del VOD
+func Get(ctx context.Context, idvideoteca string) (result Response, err error) {
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	URLPAREMETRIZZATA := searchURLbeginning + idvideoteca + searchURLending
+
+	//fmt.Println(URLPAREMETRIZZATA)
+
+	resp, err := http.Get(URLPAREMETRIZZATA)
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+
+	// Elimina il carattere di carriage return di windons che
+	// ogni tanto si trova nella descrizione dei vod.
+	bodyString2 := strings.ReplaceAll(bodyString, "\u003f", "")
+
+	err = json.Unmarshal([]byte(bodyString2), &result)
+	if err != nil {
+		log.Println(err)
+	}
+
+	
+	return result, err
+
+}
+
+//  0x000A, 0x000B, 0x000C, 0x000D, 0x0085, 0x2028, 0x2029
